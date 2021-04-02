28830cd67e491b4d038d327e8aac6e00d8f83665
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-04-02 06:05:49 +0200

    Create Queue-via-Stack.java

---
+++ Java/Data Structures/Stack-Queues/Queue-via-Stack.java
@@ -0,0 +1,38 @@
+class MyQueue {
+    ArrayList<Integer>stack;
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        this.stack = new ArrayList<>(); 
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        this.stack.add(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        int popped = this.stack.get(0);
+        this.stack.remove(0);
+        return popped;
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        return this.stack.get(0);
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return this.stack.size()==0;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
