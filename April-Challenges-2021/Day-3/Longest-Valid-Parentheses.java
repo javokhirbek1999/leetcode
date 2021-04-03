65225e382f3621996182bd3f95fd19b44db214a0
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-04-03 18:48:30 +0200

    Create Longest-Valid-Parentheses.java

---
+++ April-Challenges-2021/Day-3/Longest-Valid-Parentheses.java
@@ -0,0 +1,21 @@
+ 
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer>stack = new Stack<>();
+        stack.push(-1);
+        int max_pars = 0;
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i)=='('){
+                stack.push(i);
+            }else{
+                stack.pop();
+                if (stack.size()==0){
+                    stack.push(i);
+                }else{
+                    max_pars = Math.max(max_pars,i-stack.peek());
+                }
+            }
+        }
+        return max_pars;
+    }
+}
