e82a0b79c2a04102b8f26dd138e5183e85756321
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-04-03 16:38:30 +0200

    Time: O(n); Space: O(n);

---
+++ Python/Data Structures/Trees/Binary-Tree/Binary-Tree-Preorder-Traversal.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.values = []
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return         
+        if root.val is not None:
+            self.values.append(root.val)
+        self.preorderTraversal(root.left)
+        self.preorderTraversal(root.right)
+        
+        return self.values
