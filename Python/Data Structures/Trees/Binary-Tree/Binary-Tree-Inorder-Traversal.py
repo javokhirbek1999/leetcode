e2be51faa166e1338e95301d5931fdfeb476fab7
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-04-04 15:59:17 +0200

    Binary Tree Inorder Traversal

---
+++ Python/Data Structures/Trees/Binary-Tree/Binary-Tree-Inorder-Traversal.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.items = []
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return 
+        self.inorderTraversal(root.left)
+        if root:
+            self.items.append(root.val)
+        self.inorderTraversal(root.right)
+        return self.items 
+            
