5f9ab05192f8c659e45930cfd1f426b47f6beb7e
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-04-05 15:58:55 +0200

    Create Global-And-Local-Inversion.java

---
+++ April-Challenges-2021/Day-5/Global-And-Local-Inversion.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isIdealPermutation(int[] A) {
+        int max = 0;
+        for(int i = 0; i<A.length-2; i++){
+            max = Math.max(max,A[i]);
+            if(max>A[i+2])return false;
+        }
+        return true;
+    }
+}
