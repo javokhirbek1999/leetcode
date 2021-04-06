64a3dca2dd7d78361804d793b46f5e562c0352d3
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-04-06 07:09:44 +0200

    Time: O(log N) | Space: O(log N)

---
+++ C++/Data Structures/Trees/BinaryTrees/Insert-into-Binary-Search-Tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==nullptr){
+            root = new TreeNode(val);
+        }else if(val<=root->val){
+            if(root->left == nullptr){
+                root->left = new TreeNode(val);
+            }else{
+                insertIntoBST(root->left,val);
+            }
+        }else{
+            if(root->right==nullptr){
+                root->right = new TreeNode(val);
+            }else{
+                insertIntoBST(root->right,val);
+            }
+        }
+        return root;
+    }
+};
