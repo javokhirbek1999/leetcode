0ecc17b2fc4008dca7f49efe17c2eeb05d79f8fe
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-05-02 06:37:37 +0200

    Create Richest-Customer-Wealth.py

---
+++ Python/Algorithms/Richest-Customer-Wealth.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        for i in range(len(accounts)):
+            accounts[i] = sum(accounts[i])
+        return max(accounts)
+            
