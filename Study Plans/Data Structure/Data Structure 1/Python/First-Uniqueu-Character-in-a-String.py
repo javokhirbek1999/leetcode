7fd4225d7b135e6c3bc995ce83e65c3269ffc349
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2022-04-16 16:23:49 +0200

    Create First-Uniqueu-Character-in-a-String.py

---
+++ Study Plans/Data Structure/Data Structure 1/Python/First-Uniqueu-Character-in-a-String.py
@@ -0,0 +1,13 @@
+from collections import Counter
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        
+        c = Counter(s)
+        
+        for i in range(len(s)):
+            if c[s[i]] == 1:
+                return i
+        return -1
+                
+        
+        
